10109e25b4c6b0f69fac26bf1308d02ba5c70fa1
robert <lihungte96@gmail.com>
2018-04-17 20:18:46 -0700

    mid-term exam

--- cal/cal.cpp
+++ cal/cal.cpp
@@ -1,33 +1,34 @@
-# include <iostream>
+#include <iostream>
+//#include <stdexcept>
 using namespace std;
 
 int cal(char op, float num1, float num2)
 {
-    int ans =0;
+	int ans =0;
 
-    switch(op)
-    {
-        case '+':
-            ans = num1+num2;
-            break;
+	switch(op)
+	{
+		case '+':
+			ans = num1+num2;
+			break;
 
-        case '-':
-            ans = num1-num2;
-            break;
+		case '-':
+			ans = num1-num2;
+			break;
 
-        case '*':
-            ans = num1*num2;
-            break;
+		case '*':
+			ans = num1*num2;
+			break;
 
-        case '/':
-            ans = num1/num2;
-            break;
+		case '/':
+			ans = num1/num2;
+			break;
 
-        default:
-            // If the operator is other than +, -, * or /,
-		    //error message is shown
-            cout << "Error! operator is not correct" << endl;
-            break;
-    }
+		default:
+			// If the operator is other than +, -, * or /,
+			//error message is shown
+			throw std::out_of_range("error op: " + op);
+			break;
+	}
 	return ans;
 }
--- cal/cal_unittest.cpp
+++ cal/cal_unittest.cpp
@@ -1,6 +1,18 @@
 #include "gtest/gtest.h"
 #include "cal.h"
 TEST(cal, add){
-    EXPECT_EQ(cal('+', 2, 3), 5);
-   EXPECT_EQ(cal('+', 0, 3), 3);
+	EXPECT_EQ(cal('+', 2, 3), 5);
+}
+
+TEST(cal, sub){
+}
+
+TEST(cal, mul){
+}
+
+TEST(cal, div){
+}
+
+TEST(cal, unknown){
+	EXPECT_ANY_THROW(cal('%', 0, 3));
 }
