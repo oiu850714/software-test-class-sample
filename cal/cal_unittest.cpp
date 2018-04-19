5194b554052edb546eb17c0772f17a2369a7a2e4
oiu850714 <oiu850714@gmail.com>
2018-04-19 13:59:45 +0800

    update cal_unittest.cpp and Makefile

--- cal/cal_unittest.cpp
+++ cal/cal_unittest.cpp
@@ -1,18 +1,11 @@
-#include "gtest/gtest.h"
 #include "cal.h"
-TEST(cal, add){
-	EXPECT_EQ(cal('+', 2, 3), 5);
-}
+#include "gtest/gtest.h"
+TEST(cal, add) { EXPECT_EQ(cal('+', 2, 3), 5); }
 
-TEST(cal, sub){
-}
+TEST(cal, sub) { EXPECT_EQ(cal('-', 2, 3), -1); }
 
-TEST(cal, mul){
-}
+TEST(cal, mul) { EXPECT_EQ(cal('*', 2, 3), 6); }
 
-TEST(cal, div){
-}
+TEST(cal, div) { EXPECT_EQ(cal('/', 4, 2), 2); }
 
-TEST(cal, unknown){
-	EXPECT_ANY_THROW(cal('%', 0, 3));
-}
+TEST(cal, unknown) { EXPECT_ANY_THROW(cal('%', 0, 3)); }
